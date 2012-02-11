7cd95d65b50284a4a172833c27ec1e463013414e
Nicolas Ruflin <spam@ruflin.com>
2012-02-11 12:32:41 +0100

    Code cleanup. Add security check for loadFile

--- lib/xBoilerplate.php
+++ lib/xBoilerplate.php
@@ -31,16 +31,30 @@ class xBoilerplate {
 	public function __construct($uri, $params) {
 		$uriParse = parse_url($_SERVER['REQUEST_URI']);
 		$this->_params = $params;
-		$this->_basePath = dirname(__DIR__) . PATH_SEPARATOR;
+		$this->_basePath = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 	}
 
+	/**
+	 * Renders the page template
+	 *
+	 * @return string Returns the rendered page
+	 */
 	public function render() {
 		return $this->loadLayout('template.php');
 	}
 
-	public function loadPage($category = '', $page = '') {
-		$category = $category?$category:$this->getCategory();
-		$page = $page?$page:$this->getPage();
+	/**
+	 * Loads the content for a specific page
+	 *
+	 * If the page is not found, content of page page-not-found is returned
+	 *
+	 * @param string $category OPTIONAL Takes category from params if not set
+	 * @param string $page OPTIONAL Takes page from params if not set
+	 * @return string Page content
+	 */
+	public function loadPage($category = null, $page = null) {
+		$category = $category ? $category : $this->getCategory();
+		$page = $page ? $page : $this->getPage();
 
 		try {
 			$content = $this->loadContent($category . '/' . $page . '.php');
@@ -50,13 +64,30 @@ class xBoilerplate {
 		return $content;
 	}
 
+	/**
+	 * @param string $file Path to content file
+	 * @return string Content of file
+	 */
 	public function loadContent($file) {
-		$file = '../content/' . $file;
+		$file = 'content/' . $file;
 		return $this->_loadFile($file);
 	}
 
+	/**
+	 * Loads a file relative to the base path
+	 *
+	 * @param strin $file Path to file
+	 * @return string
+	 * @throws Exception If invalid file
+	 */
 	protected function _loadFile($file) {
-		// TODO: Secure path
+		// TODO: add more security checks
+		if (strpos($file, '..') !== false || substr($file, 0, 1) == '/') {
+			throw new Exception('Invalid file path:' . $file);
+		}
+
+		$file = $this->_basePath . $file;
+
 		if (!file_exists($file)) {
 			throw new Exception('File does not exist: ' . $file);
 		}
@@ -69,6 +100,12 @@ class xBoilerplate {
 		return $content;
 	}
 
+	/**
+	 * Returns the page param. Can be overwritten by first param
+	 *
+	 * @param string $page OPTIONAL
+	 * @return mixed|null|string
+	 */
 	public function getPage($page = '') {
 		$p = $this->_getParam('p');
 
@@ -83,23 +120,6 @@ class xBoilerplate {
 		$p = $this->_filterParam($p);
 
 		return $p;
-	}
-
-	/**
-	 * @param string $name Param to read out
-	 * @return mixed|null|string
-	 */
-	protected function _getParam($name) {
-		$param = null;
-		if (isset($this->_params[$name])) {
-			$param = $this->_params[$name];
-		}
-		$param = $this->_filterParam($param);
-		return $param;
-	}
-	
-	public function loadLayout($name) {
-		return $this->_loadFile('../layout/' . $name);
 	}
 
 	public function getCategory($category = '') {
@@ -118,6 +138,30 @@ class xBoilerplate {
 
 		return $c;
 	}
+
+	/**
+	 * Loads layout files
+	 *
+	 * @param string $name Loads a layout file
+	 * @return string Layout content
+	 */
+	public function loadLayout($name) {
+		return $this->_loadFile('layout/' . $name);
+	}
+
+	/**
+	 * @param string $name Param to read out
+	 * @return mixed|null|string
+	 */
+	protected function _getParam($name) {
+		$param = null;
+		if (isset($this->_params[$name])) {
+			$param = $this->_params[$name];
+		}
+		$param = $this->_filterParam($param);
+		return $param;
+	}
+
 
 	protected function _filterParam($param) {
 		// For security reasons
@@ -150,6 +194,7 @@ class xBoilerplate {
 
 	public function getActive($c = '', $p = '') {
 
+		// Remove get params
 		if (empty($p) && isset($_GET['c'])) {
 			return $c == $_GET['c']?'active':'';
 		}
