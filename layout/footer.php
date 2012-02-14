ba3c906d7993a063f00fe3490bbc4489d01df8ff
Martin Saidler jr <saidler@netvision.ch>
2012-02-14 16:06:03 +0100

    Add of reload button

--- layout/footer.php
+++ layout/footer.php
@@ -1,3 +1,10 @@
 	<script type="text/javascript" src="/js/libs/jquery-1.7.1.min.js"></script>
+	<div id="panel">
+		<div class="buttons">
+			<span>ACTIONS</span> ·
+			<a href="#" onclick="window.location.reload(true);">Reload without cache</a> ·
+			<a href="#" onclick="document.getElementById('panel').style.display='none';">X</a>
+		</div>
+	</div>
 </body>
 </html>
