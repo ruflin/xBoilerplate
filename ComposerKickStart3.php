b8028af7ecbb7e44b5a477ba9619bea1fc7e276b
Oliver Tupman <oliver.tupman@centralway.com>
2012-06-08 15:38:56 +0200

    no message

---
+++ ComposerKickStart3.php
@@ -0,0 +1,15 @@
+<?php
+namespace otupman;
+/**
+ * @author Oliver Tupman <oliver.tupman@centralway.com>
+ * Date: 07/06/2012
+ * Time: 17:46
+ */
+use Composer\Script\Event;
+
+class ComposerKickStart
+{
+    public static function postInstall(Event $event) {
+        $event->getIO()->ask("Is this working?");
+    }
+}
